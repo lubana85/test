20622f327ad36c858dd372ab8190e8962381276c
lubana85 <32568076+lubana85@users.noreply.github.com>
2017-10-07 05:13:25 +0400

    Update tree.js

--- tree.js
+++ tree.js
@@ -7,13 +7,15 @@ function readCSV(fileName)
 	var reader = new FileReader();
 	reader.onload = function (e) {
 	
-		var rows = e.target.result.split("\n");
-		alert(rows.length);
+		var text = e.target.result; alert(text);
+		var rows = text.split("\n");
+		
 
 		for (var i = 0; i < rows.length; i++) {
 			alert( rows[i].split(","));
 		}
 	}
 	reader.readAsText(fileName);
+
 	
 }
