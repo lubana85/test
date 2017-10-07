139fc95625d03d87756eb241fe4df184386f5759
lubana85 <32568076+lubana85@users.noreply.github.com>
2017-10-07 07:04:51 +0400

    Update tree.js

--- tree.js
+++ tree.js
@@ -4,17 +4,34 @@
 function readCSV()
 {
 	
+	var tree ;
+	
 	var fileUpload = document.getElementById("csv");
 	var reader = new FileReader();
 	reader.readAsText(fileUpload.files[0]);
 	
 	reader.onload = function (e) {
-		var text = e.target.result; alert(text);
+		var text = e.target.result; 
 		var rows = text.split("\n");
 		
 
 		for (var i = 0; i < rows.length; i++) {
-			alert( rows[i].split(","));
+			
+			// For each line in the CSV File:
+			// insert data into Tree
+			// -------------------------------
+			
+			
+			var devices = rows[i].split(",");
+			var x = devices[0];
+			var y = devices[1];
+			tree[x][y] = devices[2];
+			
+			alert(tree[x][y]);
+			
+			// -------------------------------
+			
+			
 		}
 	}
 	
@@ -24,19 +41,6 @@ function readCSV()
 		}
 		
 	}
-	/*
-	if (window.FileReader) {
-		
-		
-		
-	
-	
-	}
-	else
-	{
-		alert("The browser doesn't support file reader");
-	}
-	*/
 	
 	
 }
